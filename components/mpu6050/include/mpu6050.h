0e97c8a98e8516401c525fb4eff9a5b7c02398fb
abhishek2935 <patil.abhishek2903@gmail.com>
2026-05-14 21:29:09 +0530

    check point. todo : create good file structure

---
+++ components/mpu6050/include/mpu6050.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+
+typedef struct 
+{
+    int16_t ax , ay, az ; 
+    int16_t gx,gy,gz ; 
+    uint32_t timeStamp ; 
+}imu_struct_t;
---
+++ components/mpu6050/mpu6050.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "mpu6050.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "driver/i2c_master.h"
+
+void func(void)
+{
+    
+}
